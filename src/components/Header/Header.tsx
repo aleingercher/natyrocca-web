3ad13709dc674e68dde41ce8063c6e448ad8bc39
Ale Ingercher <aleingercher@gmail.com>
2025-01-12 14:36:03 -0300

    correcciones pre deploy

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const Header: React.FC = () => {
