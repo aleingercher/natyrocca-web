12c94e9f567f0d2166bc5b8d6d12c75e9887b001
Ale Ingercher <aleingercher@gmail.com>
2025-01-15 00:32:29 -0300

    Subo las imagenes a public

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import styles from './Home.module.scss';
+import React from 'react'
+import styles from './Home.module.scss'
 
 const Home: React.FC = () => {
-  return (
-    <div className={styles.home}>
-        <div className={styles.left}>
-            <img src="/src/assets/images/portada1.jpg" alt="logo" />
-            <p>Página en construcción</p>
-        </div>
-        <div className={styles.right}>
-            
-        </div>
-    </div>
-  );
-};
+	return (
+		<div className={styles.home}>
+			<div className={styles.left}>
+				<img src='/assets/images/portada1.jpg' alt='portada' />
+				<p>Página en construcción</p>
+			</div>
+			<div className={styles.right}></div>
+		</div>
+	)
+}
 
-export default Home;
+export default Home
