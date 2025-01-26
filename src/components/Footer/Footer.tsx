5698e8cdb8efb89e058d02f67d84857db40dad15
Ale Ingercher <aleingercher@gmail.com>
2025-01-26 18:38:59 -0300

    tambien corregimos un error tipografico en la data de la BIO

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from './Footer.module.scss';
 const Footer: React.FC = () => {
     return (
         <footer className={styles.footer}>
-            <a href="https://www.instagram.com/yourprofile" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/naty.rocca" target="_blank" rel="noopener noreferrer">
                 <FaInstagram className={styles.footer__logo} />
             </a>
             <a href="mailto:natyrocca@gmail.com" className={styles.footer__email}>natyrocca@gmail.com</a>
